b1939e68258d890cdecf34847602d4db1d5c3d45
rsmounika-dev <rasamsettymounika123@gmail.com>
2026-08-21 13:11:18 +0530

    velloris

--- init/index.js
+++ init/index.js
@@ -1,26 +1,49 @@
-const mongoose=require('mongoose')
+// const mongoose=require('mongoose')
 
-const initData=require("./data.js")
-const listing=require("../models/listing.js");
+// const initData=require("./data.js")
+// const listing=require("../models/listing.js");
 
 
 
+
+// async function main() {
+//    mongoose.connect("");
+// }
+// main().then(()=>{
+//     console.log("successful connection to db");
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+
+// const initDB =async()=>{
+//    await listing.deleteMany({});
+//    //initData.data.map((obj)=>({...obj,owner:}))
+//    await listing.insertMany(initData.data);
+//    console.log("data was initialized")
+// }
+
+// initDB();
+
+
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb+srv://mounika:mounika@cluster0.2q38qw5.mongodb.net/velloris?appName=Cluster0";
 
 async function main() {
-    mongoose.connect("mongodb://127.0.0.1:27017/velloris");
-}
-main().then(()=>{
+    await mongoose.connect(MONGO_URL);
     console.log("successful connection to db");
-})
-.catch((err)=>{
-    console.log(err);
-})
 
-const initDB =async()=>{
-   await listing.deleteMany({});
-   //initData.data.map((obj)=>({...obj,owner:}))
-   await listing.insertMany(initData.data);
-   console.log("data was initialized")
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+
+    console.log("data was initialized");
+
+    await mongoose.connection.close();
 }
 
-initDB();
+main().catch((err) => {
+    console.log(err);
+});
